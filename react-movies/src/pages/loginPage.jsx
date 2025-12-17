10544532df9af2360fb9b9999a728b333c0310f6
dianaarutcheva <20106796@mail.wit.ie>
2025-12-17 15:40:22 +0000

    Added simple signupPage.jsx

---
+++ react-movies/src/pages/loginPage.jsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from "react";
+import { Navigate, useLocation, Link } from "react-router";
+import { AuthContext } from "../contexts/authContext";
+
+const LoginPage = () => {
+  const context = useContext(AuthContext);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  let location = useLocation();
+
+  const login = () => {
+    context.authenticate(userName, password);
+  };
+
+  const { from } = location.state
+    ? { from: location.state.from.pathname }
+    : { from: "/" };
+
+  if (context.isAuthenticated === true) {
+    return <Navigate to={from} />;
+  }
+
+  return (
+    <>
+      <h2>Login page</h2>
+      <input placeholder="user name" onChange={e => setUserName(e.target.value)} />
+      <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={login}>Log in</button>
+      <p>Not registered? <Link to="/signup">Sign Up</Link></p>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ react-movies/src/pages/signupPage.jsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from "react";
+import { Navigate } from "react-router";
+import { AuthContext } from "../contexts/authContext";
+
+const SignUpPage = () => {
+  const context = useContext(AuthContext);
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [registered, setRegistered] = useState(false);
+
+
+  // Register a new user
+  const register = async () => {
+    const result = await context.register(userName, password);
+    setRegistered(result);
+  };
+
+
+   // Redirect to login page if registration is successful
+  if (registered === true) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <>
+      <h2>SignUp page</h2>
+      <input placeholder="user name" onChange={e => setUserName(e.target.value)} />
+      <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={register}>Register</button>
+    </>
+  );
+};
+
+export default SignUpPage;
